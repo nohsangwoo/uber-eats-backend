36ccc8a25673eaaef6b04db59a2dce70240d1a5e
nohsangwoo <nsgr12@naver.com>
2021-01-23 01:38:28 +0900

    windows update

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -2,15 +2,26 @@ import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
 import { Restaurant } from '../entities/restaurant.entity';
 
+// @ArgsType을 쓸수도있음
+// 불러올때 @Args()로 비워두고 사용
+// ex createRestaurant(name:"",coverImg:"",address:"") 이런형식으로 바로 사용가능
+
 @InputType()
+// DTO는 graphl에서 전달받거나 return 하는 값의 validation이다.
+// 설정된 값의 조건에 따라서만 input과 output이 엄격하게 동작하도록 설정하는것
+// 여기선 그 validation해주는 class의 이름은 CreateRestaurantInput 이고
+// 그 구성은 직접 써도 되지만
+// restaurant.entity 에서 몇개를 불러와서 사용할것이다 (picktype)
 export class CreateRestaurantInput extends PickType(Restaurant, [
   'name',
   'coverImg',
   'address',
 ]) {
-  @Field(type => String)
+  // 추가로 string type의 categoryName 또한 validation 도 validation 목록에 추가
+  @Field(type => String) //for graphql
   categoryName: string;
 }
 
+// 위와 같은 개념이지만 CoreOutput의 모든 내용을 불러와서 Validation 목록을 만듬
 @ObjectType()
 export class CreateRestaurantOutput extends CoreOutput {}
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -6,13 +6,16 @@ import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
 import { Category } from './cetegory.entity';
 
 @InputType('RestaurantInputType', { isAbstract: true })
-@ObjectType()
-@Entity()
+// objecttype은 자동으로 스키마를 빌드하기위해 사용하는 graphql decorator임
+@ObjectType() //for graphql
+// typeorm(DB) 테이블의 모델 이라고 선언
+@Entity() // for type orm
 export class Restaurant extends CoreEntity {
-  @Field(type => String)
-  @Column()
-  @IsString()
-  @Length(5)
+  @Field(type => String) //for graphql
+  @Column() //for typeorm
+  @IsString() //for validaton
+  // 최소 5글자 이상이어야함
+  @Length(5) //for validaton
   name: string;
 
   @Field(type => String)
@@ -20,11 +23,13 @@ export class Restaurant extends CoreEntity {
   @IsString()
   coverImg: string;
 
+  // 값을 채워넣지 않으면 기본값으로 '강남'을 채워넣겠다는 뜻
   @Field(type => String, { defaultValue: '강남' })
   @Column()
   @IsString()
   address: string;
 
+  //nullable은 이 값이 필수로 채워지지 않아도 된다라는 뜻
   @Field(type => Category, { nullable: true })
   @ManyToOne(
     type => Category,
